8dda3d8e1be9f599c1f0d635c9a7ee5989380220
Suryadipta <gremlin.sarkar@gmail.com>
2022-01-11 11:33:37 +0530

    added poster and done positioning

--- src/components/ShowData.jsx
+++ src/components/ShowData.jsx
@@ -1,8 +1,19 @@
 function ShowData({data}) {
     return (
         <div className="moviedataContainer">
-            <h1>{data.Title}</h1>
-            <h2>Movie pic here</h2>
+            <div>
+                <h1 className="movieTitle">{data.Title}</h1>
+            </div>
+            <div className="movieDatasubContainer">
+            <div className="column1">
+                <img src={data.Poster} alt="img here" />
+            </div>
+            <div className="column2">
+                <h3>Director : </h3>{data.Director}
+                <h3>Cast : </h3>{data.Actors}
+                <h3>Plot : </h3>{data.Plot}
+            </div>
+            </div>
         </div>
     )
 }
